9566c14a2a2da41b580daffeb68722d2e2c41a8a
Abhay Pathak <123934582+abhipathak1@users.noreply.github.com>
2023-07-11 02:53:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,63 +1,64 @@
-// Smooth scroll to section
-function smoothScroll(target, duration) {
-    const targetSection = document.querySelector(target);
-    const targetPosition = targetSection.getBoundingClientRect().top;
-    const startPosition = window.pageYOffset;
-    const distance = targetPosition - startPosition;
-    let startTime = null;
-  
-    function scrollAnimation(currentTime) {
-      if (startTime === null) startTime = currentTime;
-      const timeElapsed = currentTime - startTime;
-      const scrollY = ease(timeElapsed, startPosition, distance, duration);
-      window.scrollTo(0, scrollY);
-      if (timeElapsed < duration) requestAnimationFrame(scrollAnimation);
-    }
-  
-    function ease(t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) return c / 2 * t * t + b;
-      t--;
-      return -c / 2 * (t * (t - 2) - 1) + b;
-    }
-  
-    requestAnimationFrame(scrollAnimation);
-  }
-  
-  // Navbar links smooth scroll
-  const navLinks = document.querySelectorAll('#nav-menu a');
-  navLinks.forEach(link => {
-    link.addEventListener('click', e => {
-      e.preventDefault();
-      const targetSection = link.getAttribute('href');
-      smoothScroll(targetSection, 1000); // Set the duration (in milliseconds) as per your preference
-    });
-  });
+// // Smooth scroll to section
+// function smoothScroll(target, duration) {
+//   const targetSection = document.querySelector(target);
+//   if (targetSection) {
+//     const targetPosition = targetSection.getBoundingClientRect().top;
+//     const startPosition = window.pageYOffset;
+//     const distance = targetPosition - startPosition;
+//     let startTime = null;
 
-  
-  const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-const projectContainer = document.querySelector('.project-card-container');
+//     function scrollAnimation(currentTime) {
+//       if (startTime === null) startTime = currentTime;
+//       const timeElapsed = currentTime - startTime;
+//       const scrollY = ease(timeElapsed, startPosition, distance, duration);
+//       window.scrollTo(0, scrollY);
+//       if (timeElapsed < duration) requestAnimationFrame(scrollAnimation);
+//     }
 
-let scrollPosition = 0;
-const projectWidth = 320; // Adjust this value to match the width of each project card
-const projectsToShow = 3; // Adjust this value to change the number of projects displayed at a time
+//     function ease(t, b, c, d) {
+//       t /= d / 2;
+//       if (t < 1) return c / 2 * t * t + b;
+//       t--;
+//       return -c / 2 * (t * (t - 2) - 1) + b;
+//     }
 
-prevBtn.addEventListener('click', scrollProjects.bind(null, 'prev'));
-nextBtn.addEventListener('click', scrollProjects.bind(null, 'next'));
+//     requestAnimationFrame(scrollAnimation);
+//   }
+// }
 
-function scrollProjects(direction) {
-  const containerWidth = projectContainer.offsetWidth;
-  const scrollAmount = projectWidth * projectsToShow;
-  const maxScroll = projectContainer.scrollWidth - containerWidth;
+// // Navbar links smooth scroll
+// const navLinks = document.querySelectorAll('#nav-menu a');
+// navLinks.forEach(link => {
+//   link.addEventListener('click', e => {
+//     e.preventDefault();
+//     const targetSection = link.getAttribute('href');
+//     smoothScroll(targetSection, 1000); // Set the duration (in milliseconds) as per your preference
+//   });
+// });
 
-  if (direction === 'prev') {
-    scrollPosition -= scrollAmount;
-    scrollPosition = Math.max(scrollPosition, 0);
-  } else {
-    scrollPosition += scrollAmount;
-    scrollPosition = Math.min(scrollPosition, maxScroll);
-  }
+// const prevBtn = document.getElementById('prevBtn');
+// const nextBtn = document.getElementById('nextBtn');
+// const projectContainer = document.querySelector('.project-card-container');
 
-  projectContainer.style.transform = `translateX(-${scrollPosition}px)`;
-}
+// let scrollPosition = 0;
+// const projectWidth = 320; // Adjust this value to match the width of each project card
+// const projectsToShow = 3; // Adjust this value to change the number of projects displayed at a time
+
+// prevBtn.addEventListener('click', scrollProjects.bind(null, 'prev'));
+// nextBtn.addEventListener('click', scrollProjects.bind(null, 'next'));
+
+// function scrollProjects(direction) {
+//   const containerWidth = projectContainer.offsetWidth;
+//   const scrollAmount = projectWidth * projectsToShow;
+//   const maxScroll = projectContainer.scrollWidth - containerWidth;
+
+//   if (direction === 'prev') {
+//     scrollPosition -= scrollAmount;
+//     scrollPosition = Math.max(scrollPosition, 0);
+//   } else {
+//     scrollPosition += scrollAmount;
+//     scrollPosition = Math.min(scrollPosition, maxScroll);
+//   }
+
+//   projectContainer.style.transform = `translateX(-${scrollPosition}px)`;
+// }
