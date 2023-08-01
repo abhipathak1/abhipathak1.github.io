d741f1b179d543304a29170a2b92e844ee3c231e
Abhay Pathak <123934582+abhipathak1@users.noreply.github.com>
2023-08-01 14:50:05 +0530

    Portfolio

--- index.js
+++ index.js
@@ -78,6 +78,48 @@ navLinks.forEach(link => {
 });
 
 
+
+
+
+// dark mode toggle 
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('change', function () {
+  if (darkModeToggle.checked) {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
+
+function enableDarkMode() {
+  body.classList.add('dark-mode');
+  localStorage.setItem('darkModeEnabled', true);
+}
+
+function disableDarkMode() {
+  body.classList.remove('dark-mode');
+  localStorage.setItem('darkModeEnabled', false);
+}
+
+// Check if dark mode was previously enabled
+const darkModeEnabled = localStorage.getItem('darkModeEnabled');
+if (darkModeEnabled && darkModeEnabled === 'true') {
+  darkModeToggle.checked = true;
+  enableDarkMode();
+}
+else {
+  darkModeToggle.checked = false;
+  disableDarkMode();
+}
+
+
+
+
+
+
+
 // intro text
 
 const introText = "Java Backend Developer";
@@ -86,4 +128,7 @@ const introElement = document.getElementById("user-detail-intro");
 introElement.textContent = introText;
 introElement.classList.add("typing-animation");
 
-typeText();
+
+
+
+
